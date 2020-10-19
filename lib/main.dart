f6db9c01c97864cfce630eb4f4f9864ac71a193e
reika178 <rechiko178@gmail.com>
2020-10-20 07:33:02 +0900

    データベースから取得して表示できた！
    10/20 ダウンロードしたのを貼り付ける

--- lib/main.dart
+++ lib/main.dart
@@ -26,68 +26,69 @@ class MyFirestorePage extends StatefulWidget {
 }
 
 class _MyFirestorePageState extends State<MyFirestorePage> {
+//   @override
+// Widget build(BuildContext context) {
+//   return StreamBuilder(
+//     stream: Firestore.instance.collection('users').document('id_1').snapshots(),
+//     builder: (context, snapshot) {
+//       return Text(snapshot.data["word"]);
+//     },
+//   );
+// }
+  List<DocumentSnapshot> documentList = [];
+
+  String orderDocumentInfo = '';
+
   @override
-Widget build(BuildContext context) {
-  return StreamBuilder(
-    stream: Firestore.instance.collection('users').document('id_1').snapshots(),
-    builder: (context, snapshot) {
-      return Text(snapshot.data["word"]);
-    },
-  );
-}
-  // List<DocumentSnapshot> documentList = [];
-
-  // String orderDocumentInfo = '';
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: Center(
-  //       child: Column(
-  //         children: <Widget>[
-  //           RaisedButton(
-  //             child: Text('コレクション＋ドキュメント作成'),
-  //             onPressed: () async {
-  //               await Firestore.instance
-  //                   .collection('users')
-  //                   .document('id_1')
-  //                   .setData({'word': 'hello_world'});
-  //             },
-  //           ),
-  //           RaisedButton(
-  //             child: Text('ドキュメント一覧獲得'),
-  //             onPressed: () async {
-  //               final snapshot = 
-  //                   await Firestore.instance.collection('users').getDocuments();
-  //               setState(() {
-  //                 documentList = snapshot.documents;
-  //               });
-  //             },
-  //           ),
-  //           Column(
-  //             children: documentList.map((document) {
-  //               return ListTile(
-  //                 title: Text('${document['word']}'),
-  //               );
-  //             }).toList(),
-  //           ),
-  //           RaisedButton(
-  //             child: Text('ドキュメントを指定して取得'),
-  //             onPressed: () async {
-  //               final document = await Firestore.instance
-  //                   .collection('users')
-  //                   .document('id_1')
-  //                   .get();
-  //               setState(() {
-  //                 orderDocumentInfo = 
-  //                     '${document['word']}';
-  //               });
-  //             },
-  //           ),
-  //           ListTile(title: Text(orderDocumentInfo)),
-  //         ],
-  //       ),
-  //     ),
-  //   );
-  // }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Firestore hello_world')),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text('コレクション＋ドキュメント作成'),
+              onPressed: () async {
+                await Firestore.instance
+                    .collection('users')
+                    .document('id_2')
+                    .setData({'word': 'こんにちは'});
+              },
+            ),
+            RaisedButton(
+              child: Text('ドキュメント一覧獲得'),
+              onPressed: () async {
+                final snapshot = 
+                    await Firestore.instance.collection('users').getDocuments();
+                setState(() {
+                  documentList = snapshot.documents;
+                });
+              },
+            ),
+            Column(
+              children: documentList.map((document) {
+                return ListTile(
+                  title: Text('${document['word']}'),
+                );
+              }).toList(),
+            ),
+            RaisedButton(
+              child: Text('ドキュメントを指定して取得'),
+              onPressed: () async {
+                final document = await Firestore.instance
+                    .collection('users')
+                    .document('id_1')
+                    .get();
+                setState(() {
+                  orderDocumentInfo = 
+                      '${document['word']}';
+                });
+              },
+            ),
+            ListTile(title: Text(orderDocumentInfo)),
+          ],
+        ),
+      ),
+    );
+  }
 }
