7955a99e79011f25e8c9288011ffc2136e097c13
reika178 <rechiko178@gmail.com>
2020-10-21 13:40:42 +0900

    なんとかデータ取得、表示できた
    10/21

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: MyFirestorePage(),
     );
   }
@@ -26,69 +22,85 @@ class MyFirestorePage extends StatefulWidget {
 }
 
 class _MyFirestorePageState extends State<MyFirestorePage> {
-//   @override
-// Widget build(BuildContext context) {
-//   return StreamBuilder(
-//     stream: Firestore.instance.collection('users').document('id_1').snapshots(),
-//     builder: (context, snapshot) {
-//       return Text(snapshot.data["word"]);
-//     },
-//   );
-// }
-  List<DocumentSnapshot> documentList = [];
-
-  String orderDocumentInfo = '';
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Firestore hello_world')),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('コレクション＋ドキュメント作成'),
-              onPressed: () async {
-                await Firestore.instance
-                    .collection('users')
-                    .document('id_2')
-                    .setData({'word': 'こんにちは'});
-              },
-            ),
-            RaisedButton(
-              child: Text('ドキュメント一覧獲得'),
-              onPressed: () async {
-                final snapshot = 
-                    await Firestore.instance.collection('users').getDocuments();
-                setState(() {
-                  documentList = snapshot.documents;
-                });
-              },
-            ),
-            Column(
-              children: documentList.map((document) {
-                return ListTile(
-                  title: Text('${document['word']}'),
-                );
-              }).toList(),
-            ),
-            RaisedButton(
-              child: Text('ドキュメントを指定して取得'),
-              onPressed: () async {
-                final document = await Firestore.instance
-                    .collection('users')
-                    .document('id_1')
-                    .get();
-                setState(() {
-                  orderDocumentInfo = 
-                      '${document['word']}';
-                });
-              },
-            ),
-            ListTile(title: Text(orderDocumentInfo)),
-          ],
-        ),
-      ),
+    return StreamBuilder(
+      stream: Firestore.instance.collection('users').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) return Text("loading");
+        return ListView(
+          children: snapshot.data.documents.map<Widget>(
+            (document) {
+              return Text(document["word"]);
+            },
+          ).toList(),
+        );
+      },
     );
   }
+  // List<DocumentSnapshot> documentList = [];
+
+  // String orderDocumentInfo = '';
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(title: Text('Firestore hello_world')),
+  //     body: Center(
+  //       child: Column(
+  //         children: <Widget>[
+            // RaisedButton(
+            //   child: Text('コレクション＋ドキュメント作成'),
+            //   onPressed: () async {
+            //     await Firestore.instance
+            //         .collection('users')
+            //         .document('id_2')
+            //         .setData({'word': 'こんにちは'});
+            //   },
+            // ),
+            // RaisedButton(
+            //   child: Text('ドキュメント一覧獲得'),
+            //   onPressed: () async {
+            //     final snapshot = 
+            //         await Firestore.instance.collection('users').getDocuments();
+            //     setState(() {
+            //       documentList = snapshot.documents;
+            //     });
+            //   },
+            // ),
+            // Column(
+            //   children: documentList.map((document) {
+            //     return ListTile(
+            //       title: Text('${document['word']}'),
+            //     );
+            //   }).toList(),
+            // ),
+            // RaisedButton(
+            //   child: Text('ドキュメントを指定して取得'),
+            //   onPressed: () async {
+            //     final document = await Firestore.instance
+            //         .collection('users')
+            //         .document('id_1')
+            //         .get();
+            //     setState(() {
+            //       orderDocumentInfo = 
+            //           '${document['word']}';
+            //     });
+            //   },
+            // ),
+            // ListTile(title: Text(orderDocumentInfo)),
+            // RaisedButton(
+            //   child: Text('ドキュメント削除'),
+            //   onPressed: () async {
+            //     await Firestore.instance
+            //         .collection('users')
+            //         .document('id_2')
+            //         .delete();
+            //   }
+            // )
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
